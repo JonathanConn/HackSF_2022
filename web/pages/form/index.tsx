ad121c69da073b3b2acccf414d57834c587c27e6
Jonathan Conn <jonathanconn29@gmail.com>
2022-11-05 23:10:45 -0700

    refactored, working img upload

---
+++ web/pages/form/index.tsx
@@ -0,0 +1,22 @@
+import styles from '../../styles/Home.module.css'
+import { useState } from "react";
+import UploadForm from './uploadForm';
+import MintForm from './mintForm';
+
+
+export default function Form() {
+    return (
+        <main className={styles.main}>
+            <div className={styles.container}>
+                <div className={styles.grid}>
+                    <MintForm /> 
+                </div>
+                <div className={styles.grid}>
+                    <UploadForm />
+                </div>
+            </div>
+        </main>
+
+    )
+}
+
---
+++ web/pages/form/uploadForm.tsx
@@ -0,0 +1,34 @@
+import styles from '../../styles/Home.module.css'
+import { useState } from "react";
+
+export default function UploadForm() {
+
+    const [image, setImage] = useState<File>();
+
+    const uploadToClient = async (event: any) => {
+        setImage(event.target.files[0]);
+    }
+
+    const uploadToServer = async (event: any) => {
+        // event.preventDefault();
+        const body = new FormData();
+        body.append("file", image!);
+
+        await fetch("/api/mint/uploadImg", {
+            method: "POST",
+            body
+          });``
+
+    }
+
+    return (
+
+        <form onSubmit={uploadToServer}>
+            <input id="images" type="file" onChange={uploadToClient} className={styles.card} />
+            <br></br>
+            <button className={styles.center}>Upload</button>
+        </form>
+
+    )
+}
+

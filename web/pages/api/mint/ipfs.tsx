ea5a3c369527015c88f9dc36c2a75a478acfc232
Jonathan Conn <jonathanconn29@gmail.com>
2022-11-06 02:41:26 -0800

    refactored contracts

--- web/pages/api/mint/ipfs.tsx
+++ web/pages/api/mint/ipfs.tsx
@@ -5,11 +5,12 @@ var fs = require('fs');
 
 // uploads to ipfs
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-
+    const filename = `.req.body.filename}`;
+    
     var data = new FormData();
-    data.append('file', fs.createReadStream(req.body.path));
+    data.append('file', fs.createReadStream(filename));
     data.append('pinataOptions', '{"cidVersion": 1}');
-    data.append('pinataMetadata', `{"name": "${req.body.name}"}`);
+    data.append('pinataMetadata', `{"name": "${filename}"}`);
 
     var config = {
         method: 'post',
@@ -20,11 +21,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         },
         data: data
     };
-    
-    await axios(config).then((axios_res: any) => {
-        
-        return res.status(200).json(axios_res.data);
-    })
-    
 
+    const axios_res = await axios(config);
+    return res.status(200).json(axios_res.data);
 };
